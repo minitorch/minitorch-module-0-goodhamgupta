1e3eff5b1c3b9e59d95e8602a74a876eb8dcc74b
goodhamgupta <shubhamg2208@live.com>
2023-12-21 15:48:47 +0800

    Add solutions for task 0.1 and 0.2

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -108,15 +108,19 @@ def test_sigmoid(a: float) -> None:
     * It is  strictly increasing.
     """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert(sigmoid(a) >= 0.0 and sigmoid(a) <= 1.0)
+    assert_close(1 - sigmoid(a), sigmoid(-a))
+    assert_close(sigmoid(0), 0.5)
+    assert(sigmoid(a) <= sigmoid(a + 1.0))
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a: float, b: float, c: float) -> None:
     "Test the transitive property of less-than (a < b and b < c implies a < c)"
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    if a < b and b < c:
+        assert(lt(a, c))
+
 
 
 @pytest.mark.task0_2
@@ -125,8 +129,7 @@ def test_symmetric() -> None:
     Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(2, 3), mul(3, 2))
 
 
 @pytest.mark.task0_2
@@ -135,8 +138,7 @@ def test_distribute() -> None:
     Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(2, add(3, 4)), add(mul(2, 3), mul(2, 4)))
 
 
 @pytest.mark.task0_2
@@ -144,8 +146,7 @@ def test_other() -> None:
     """
     Write a test that ensures some other property holds for your functions.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(add(2, 3), add(3, 2))
 
 
 # ## Task 0.3  - Higher-order functions
